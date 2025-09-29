f6ac1911a0a69e7a592d8b8e8113ee30f0b90a91
Yotam Ashman <tanihu2@gmail.com>
2025-09-29 17:31:21 +0300

    Initial grid with folder icon

--- src/gui.rs
+++ src/gui.rs
@@ -4,38 +4,66 @@ use crate::App;
 
 pub struct AppGui {
     app: App,
+    initialized: bool,
 }
 
 impl Default for AppGui {
     fn default() -> Self {
-        Self { app: App {} }
+        Self {
+            app: App {},
+            initialized: false,
+        }
     }
 }
 
 impl eframe::App for AppGui {
     fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+        // Lazy init
+        if !self.initialized {
+            self.initialize(ctx);
+        }
+
         self.draw_top_panel(ctx);
+        self.draw_bottom_panel(ctx);
     }
 }
 
 impl AppGui {
+    fn initialize(&mut self, ctx: &eframe::egui::Context) {
+        if self.initialized {
+            panic!("Gui initialization called twice!");
+        }
+        egui_extras::install_image_loaders(ctx);
+        self.initialized = true;
+    }
+
     fn draw_top_panel(&self, ctx: &egui::Context) {
-        egui::TopBottomPanel::top("NavBar").show(ctx, |ui| {
+        egui::TopBottomPanel::top("nav_bar").show(ctx, |ui| {
             ui.horizontal(|ui| {
-                if ui.button("LEFT").clicked() {
+                // Back/Forward buttons
+                if ui.button("<").clicked() {
                     println!("Left nav button clicked!");
                 }
 
-                if ui.button("RIGHT").clicked() {
+                if ui.button(">").clicked() {
                     println!("Right nav button clicked!");
                 }
+
                 ui.separator();
+
+                // Current path
                 ui.label("Nav path placeholder");
             })
         });
     }
 
-    fn draw_bottom_panel(&self, ui: egui::Ui) {
-        todo!();
+    fn draw_bottom_panel(&self, ctx: &egui::Context) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::Grid::new("file_grid").show(ui, |ui| {
+                ui.add(egui::Image::new(egui::include_image!(
+                    "../assets/folder_icon.svg"
+                )));
+            });
+        });
     }
 }
